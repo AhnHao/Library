ab84d7fd553b48ae848c2197c77eaf900595dbfe
AhnHao <haotrananh2@gmail.com>
2024-08-09 20:23:13 +0700

    Started

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+import url from 'url'
+import connectDB from './config/database.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const port = process.env.PORT || 8080
 
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res) => {
+    res.send('<h1>hello world</h1>')
+})
+
+connectDB()
 app.listen(port, () => {
     console.log(`sever is running on  ${port}`)
 })
